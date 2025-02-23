7964bc5299f5e1f7707d48ff2a8269253000053b
Roland Haas <rhaas@illinois.edu>
2025-02-23 12:05:13 -0500

    GRHydro: fix s2 calculation during Faber fix

--- src/GRHydro_RepRimAnd.cc
+++ src/GRHydro_RepRimAnd.cc
@@ -49,12 +49,12 @@ void CCTK_FCALL CCTK_FNAME(GRHydro_RPR_Con2Prim_pt)(
   real_t tau = *tau_in ;
 
   // Faber 2007 fix for max s2
-  real_t s2 = (    g.lo(0,0) * scon1*scon1 +
-               2 * g.lo(1,0) * scon2*scon1 +
-               2 * g.lo(2,0) * scon3*scon1 +
-                   g.lo(1,1) * scon2*scon2 +
-               2 * g.lo(1,2) * scon2*scon3 +
-                   g.lo(2,2) * scon3*scon3) * g.det;
+  real_t s2 =     g.lo(0,0) * scon1*scon1 +
+              2 * g.lo(1,0) * scon2*scon1 +
+              2 * g.lo(2,0) * scon3*scon1 +
+                  g.lo(1,1) * scon2*scon2 +
+              2 * g.lo(1,2) * scon2*scon3 +
+                  g.lo(2,2) * scon3*scon3;
   if(s2 >= tau*(tau + 2*dens)) {
     real_t corrfac = sqrt(0.98 * tau*(tau + 2*dens)/s2);
     *scon1_in = (scon1 *= corrfac);
